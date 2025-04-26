17e619776994cdfb8b6c5ce9ac6aa9829faadf0e
RaghuBadugu <raghubadugu7@gmail.com>
2025-04-26 21:48:57 +0530

    Create Jenkins-setup.sh

---
+++ Scripts/Jenkins-setup.sh
@@ -0,0 +1,5 @@
+yum install java-17-amazon-corretto.x86_64 -y
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+yum install jenkins -y
+systemctl start jenkins.service
